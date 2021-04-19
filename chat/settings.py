da86145697c2022646d85facdd5e93b634f95c00
deepak-collegedekho <deepak.jangir@collegedekho.com>
2021-04-19 15:16:44 +0530

    fix

--- chat/settings.py
+++ chat/settings.py
@@ -200,7 +200,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('https://haptik-chatapp.herokuapp.com', 6379)],
         },
     },
 }
